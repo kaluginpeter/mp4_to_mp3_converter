87f11a1bfbf012e5fc2ae9ed4a290666af359909
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-02 21:42:36 +0300

    Fixing some text errors

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import moviepy.editor
-video = moviepy.editor.VideoFileClip('Rick_Roll.mp4')
+
+video = moviepy.editor.VideoFileClip('Path to your .mp4 file')
+
+# Converting
 audio = video.audio
-audio.write_audiofile('rick.mp3')
+
+# Save audio
+audio.write_audiofile('Name your audio.mp3') # Ex: my_audio.mp3
+
+# Run file
